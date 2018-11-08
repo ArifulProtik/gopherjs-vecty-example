c04c136b0dd5cb659b7a0d8f8e3f3f651de323b2
MD Ariful Islam Protik <34741765+ArifulProtik@users.noreply.github.com>
2018-11-08 12:39:13 +0600

    1st Commit

---
+++ main.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"github.com/gopherjs/vecty"
+	"github.com/gopherjs/vecty/elem"
+	"github.com/gopherjs/vecty/prop"
+)
+
+func main() {
+	vecty.SetTitle("Simple Login Page With Vecty")
+	vecty.AddStylesheet("/style.css")
+	vecty.RenderBody(&mybody{})
+}
+
+type mybody struct {
+	vecty.Core
+}
+
+func (*mybody) Render() vecty.ComponentOrHTML {
+	return elem.Body(
+		elem.Div(
+			vecty.Markup(
+				vecty.Class("mform"),
+			),
+			elem.Input(
+				vecty.Markup(
+					prop.Type(prop.TypeText),
+					vecty.Class("inpt"),
+					prop.Placeholder("Username"),
+				),
+			),
+			elem.Input(
+				vecty.Markup(
+					prop.Type(prop.TypePassword),
+					vecty.Class("inpt"),
+					prop.Placeholder("password"),
+				),
+			),
+			elem.Input(
+				vecty.Markup(
+					prop.Type(prop.TypeSubmit),
+					prop.Value("Login"),
+					vecty.Class("sbmt"),
+				),
+			),
+			elem.Span(
+				vecty.Markup(
+					vecty.Class("s1"),
+				),
+				vecty.Text("Not a member."),
+				elem.Anchor(
+					vecty.Markup(
+						prop.Href("#"),
+					),
+					vecty.Text("Sign Up Here"),
+				),
+			),
+		),
+	)
+}
